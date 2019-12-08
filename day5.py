1010bba9c4639c56c7495268cb96af972a9e1d49
Ratana <ratanawang8@gmail.com>
2019-12-07 21:58:04 -0800

    completed day 5: part 2

--- day5.py
+++ day5.py
@@ -11,11 +11,13 @@ def add_store():
         nums[nums[index + 3]] = nums[nums[index + 1]] + nums[index + 2]
     elif temp[-3] == "1" and temp[-4] == "1":  # both parameters in immediate mode
         nums[nums[index + 3]] = nums[index + 1] + nums[index + 2]
+    if nums[index] != int(temp):
+        return index
     return 4
 
 
 def multiply_store():
-    temp = temp = "0"*(5 - len(str(nums[index]))) + str(nums[index])
+    temp = "0"*(5 - len(str(nums[index]))) + str(nums[index])
     if temp[-3] == "0" and temp[-4] == "0":  # position mode
         nums[nums[index + 3]] = nums[nums[index + 1]] * nums[nums[index + 2]]
     elif temp[-3] == "1" and temp[-4] == "0":  # 1st parameter in immediate mode
@@ -24,6 +26,8 @@ def multiply_store():
         nums[nums[index + 3]] = nums[nums[index + 1]] * nums[index + 2]
     elif temp[-3] == "1" and temp[-4] == "1":  # both parameters in immediate mode
         nums[nums[index + 3]] = nums[index + 1] * nums[index + 2]
+    if nums[index] != int(temp):
+        return index
     return 4
 
 
@@ -35,6 +39,94 @@ def store():
 def grab():
     print(nums[nums[index + 1]])
     return 2
+
+
+def jump_if_true():
+    temp = "0" * (5 - len(str(nums[index]))) + str(nums[index])
+    if temp[-3] == "0" and temp[-4] == "0":  # position mode
+        if nums[nums[index + 1]] != 0:
+            return nums[nums[index + 2]]
+    elif temp[-3] == "1" and temp[-4] == "0":  # 1st parameter in immediate mode
+        if nums[index + 1] != 0:
+            return nums[nums[index + 2]]
+    elif temp[-3] == "0" and temp[-4] == "1":  # 2nd parameter in immediate mode
+        if nums[nums[index + 1]] != 0:
+            return nums[index + 2]
+    elif temp[-3] == "1" and temp[-4] == "1":  # both parameters in immediate mode
+        if nums[index + 1] != 0:
+            return nums[index + 2]
+    return index + 3
+
+
+def jump_if_false():
+    temp = "0" * (5 - len(str(nums[index]))) + str(nums[index])
+    if temp[-3] == "0" and temp[-4] == "0":  # position mode
+        if nums[nums[index + 1]] == 0:
+            return nums[nums[index + 2]]
+    elif temp[-3] == "1" and temp[-4] == "0":  # 1st parameter in immediate mode
+        if nums[index + 1] == 0:
+            return nums[nums[index + 2]]
+    elif temp[-3] == "0" and temp[-4] == "1":  # 2nd parameter in immediate mode
+        if nums[nums[index + 1]] == 0:
+            return nums[index + 2]
+    elif temp[-3] == "1" and temp[-4] == "1":  # both parameters in immediate mode
+        if nums[index + 1] == 0:
+            return nums[index + 2]
+    return index + 3
+
+
+def less_than():
+    temp = "0" * (5 - len(str(nums[index]))) + str(nums[index])
+    if temp[-3] == "0" and temp[-4] == "0":  # position mode
+        if nums[nums[index + 1]] < nums[nums[index + 2]]:
+            nums[nums[index + 3]] = 1
+        else:
+            nums[nums[index + 3]] = 0
+    elif temp[-3] == "1" and temp[-4] == "0":  # 1st parameter in immediate mode
+        if nums[index + 1] < nums[nums[index + 2]]:
+            nums[nums[index + 3]] = 1
+        else:
+            nums[nums[index + 3]] = 0
+    elif temp[-3] == "0" and temp[-4] == "1":  # 2nd parameter in immediate mode
+        if nums[nums[index + 1]] < nums[index + 2]:
+            nums[nums[index + 3]] = 1
+        else:
+            nums[nums[index + 3]] = 0
+    elif temp[-3] == "1" and temp[-4] == "1":  # both parameters in immediate mode
+        if nums[index + 1] < nums[index + 2]:
+            nums[nums[index + 3]] = 1
+        else:
+            nums[nums[index + 3]] = 0
+    if nums[index] != int(temp):
+        return index
+    return 4
+
+
+def equals():
+    temp = "0" * (5 - len(str(nums[index]))) + str(nums[index])
+    if temp[-3] == "0" and temp[-4] == "0":  # position mode
+        if nums[nums[index + 1]] == nums[nums[index + 2]]:
+            nums[nums[index + 3]] = 1
+        else:
+            nums[nums[index + 3]] = 0
+    elif temp[-3] == "1" and temp[-4] == "0":  # 1st parameter in immediate mode
+        if nums[index + 1] == nums[nums[index + 2]]:
+            nums[nums[index + 3]] = 1
+        else:
+            nums[nums[index + 3]] = 0
+    elif temp[-3] == "0" and temp[-4] == "1":  # 2nd parameter in immediate mode
+        if nums[nums[index + 1]] == nums[index + 2]:
+            nums[nums[index + 3]] = 1
+        else:
+            nums[nums[index + 3]] = 0
+    elif temp[-3] == "1" and temp[-4] == "1":  # both parameters in immediate mode
+        if nums[index + 1] == nums[index + 2]:
+            nums[nums[index + 3]] = 1
+        else:
+            nums[nums[index + 3]] = 0
+    if nums[index] != int(temp):
+        return index
+    return 4
 
 
 nums = [3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1102, 46, 47, 225, 2, 122, 130, 224, 101, -1998, 224, 224, 4,
@@ -67,9 +159,9 @@ nums = [3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1102, 46, 47, 225, 2, 1
         223, 8, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 644, 101, 1, 223, 223, 1007, 226, 226, 224, 102, 2, 223,
         223, 1005, 224, 659, 101, 1, 223, 223, 108, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 674, 1001, 223, 1, 223,
         4, 223, 99, 226]
-index = 0
 
-while index < len(nums) - 2:
+index = 0
+while index < (len(nums) - 2):
     if nums[index] % 100 == 1:
         index += add_store()
     elif nums[index] % 100 == 2:
@@ -78,5 +170,13 @@ while index < len(nums) - 2:
         index += store()
     elif nums[index] % 100 == 4:
         index += grab()
+    elif nums[index] % 100 == 5:
+        index = jump_if_true()
+    elif nums[index] % 100 == 6:
+        index = jump_if_false()
+    elif nums[index] % 100 == 7:
+        index += less_than()
+    elif nums[index] % 100 == 8:
+        index += equals()
     elif nums[index] % 100 == 99:
         index = len(nums)

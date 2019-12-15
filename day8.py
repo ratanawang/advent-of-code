5b9ce449302e223d6e24ddd08ff92728923cb7ec
Ratana <ratanawang8@gmail.com>
2019-12-15 00:09:24 -0800

    complete day 8: part one

---
+++ day8.py
@@ -0,0 +1,22 @@
+# part one
+
+import textwrap
+from collections import Counter
+
+layers = textwrap.wrap(input(), 150)
+print(layers)
+zeros = {}
+
+for i in range(0, len(layers)):
+    count = Counter(layers[i])
+    print("count of character frequency in layer {} is: ".format(i) + str(count))
+    zeros[i] = count["0"]
+
+key_list = list(zeros.keys())
+value_list = list(zeros.values())
+
+lowest = key_list[value_list.index(min(value_list))]
+print("layer {} has the lowest number of zeros".format(lowest))
+
+count_lowest = Counter(layers[lowest])
+print("it has {} 1's and {} 2's".format(count_lowest["1"], count_lowest["2"]))

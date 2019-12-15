5bcb00fdc406a570e55dc6db2ac1c11ae987cf61
Ratana <ratanawang8@gmail.com>
2019-12-14 23:22:59 -0800

    complete day 6: part one

--- day6.py
+++ day6.py
@@ -1,37 +1,17 @@
 # part one
 # find number of direct and indirect orbits
-# objects = set()
-#
-#
-# # only create an object if it orbits another, is orbiter
-# class Object:
-#     orbits = []
-#     name = ""
-#
-#     def __init__(self, string):
-#         global objects
-#         self.name = string.split(")")[1]  # the orbiter
-#         self.orbits.append(string.split(")")[0])  # the orbited
-#         print("{} orbits {}".format(string.split(")")[1], string.split(")")[0]))
-#         objects.add(self)
-#
-#
-# while True:
-#     orbit = input()
-#     if orbit == "/exit":
-#         break
-#     else:
-#         orbited = orbit.split(")")[0]
-#         orbiter = orbit.split(")")[1]
-#         inlist = False
-#         for x in objects:
-#             if x.name == orbiter:
-#                 inlist = True
-#         if inlist:
-#             orbiter = Object(orbit)
-#         else:
-#             getattr(orbiter, "d_orbits").append(orbit.split(")")[0])
-#             print(getattr(orbiter, "d_orbits"))
+
+
+def add_stuff(amount):
+    global count
+    count += amount
+
+
+def count_stuff(key):
+    if len(objects[key]) > 0:
+        add_stuff(len(objects[key]))
+        for y in objects[key]:
+            count_stuff(y)
 
 
 objects = {}
@@ -45,19 +25,12 @@ while True:
         orbiter = orbit.split(")")[1]
         if orbiter not in objects:
             objects[orbiter] = set()
+        if orbited not in objects:
+            objects[orbited] = set()
         objects[orbiter].add(orbited)
         print("{} orbits {}".format(orbiter, objects[orbiter]))
 
 count = 0
 for x in objects:
-    count += len(objects[x])
-    for y in objects[x]:
-        if y in objects:
-            count += len(objects[y])
-            for z in objects[y]:
-                if z in objects:
-                    count += len(objects[z])
-                    for a in objects[z]:
-                        if a in objects:
-                            count += len(objects[a])
+    count_stuff(x)
 print(count)

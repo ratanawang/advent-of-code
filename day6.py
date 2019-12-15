21620729bf73f409e35a968529be4ee82b79be7b
Ratana <ratanawang8@gmail.com>
2019-12-14 22:03:47 -0800

    day 6: new approach (dictionaries)

--- day6.py
+++ day6.py
@@ -1,35 +1,63 @@
 # part one
 # find number of direct and indirect orbits
+# objects = set()
+#
+#
+# # only create an object if it orbits another, is orbiter
+# class Object:
+#     orbits = []
+#     name = ""
+#
+#     def __init__(self, string):
+#         global objects
+#         self.name = string.split(")")[1]  # the orbiter
+#         self.orbits.append(string.split(")")[0])  # the orbited
+#         print("{} orbits {}".format(string.split(")")[1], string.split(")")[0]))
+#         objects.add(self)
+#
+#
+# while True:
+#     orbit = input()
+#     if orbit == "/exit":
+#         break
+#     else:
+#         orbited = orbit.split(")")[0]
+#         orbiter = orbit.split(")")[1]
+#         inlist = False
+#         for x in objects:
+#             if x.name == orbiter:
+#                 inlist = True
+#         if inlist:
+#             orbiter = Object(orbit)
+#         else:
+#             getattr(orbiter, "d_orbits").append(orbit.split(")")[0])
+#             print(getattr(orbiter, "d_orbits"))
 
 
-class Object:
-    d_orbits = []
-    i_orbits = []
-    name = ""
-
-    def __init__(self, string):
-        self.name = string.split(")")[1]  # the orbiter
-        self.d_orbits.append(string.split(")")[0])  # the orbited
-        print("{} orbits {}".format(self.name, string.split(")")[0]))
-
-
-# converts data with numbers into readable strings
-def converter(string):
-    alphabet = list("abcdefghijklmnopqrstuvwxyz")
-    for i in string:
-        if i.isdigit():
-            string.replace(i, alphabet[i])
-    return string
-
+objects = {}
 
 while True:
     orbit = input()
     if orbit == "/exit":
         break
     else:
-        orbit = converter(orbit)
+        orbited = orbit.split(")")[0]
         orbiter = orbit.split(")")[1]
-        if not isinstance(orbiter, Object):
-            orbiter = Object(orbit)
-        else:
-            orbiter.d_orbits.append(orbit.split(")")[0])
+        if orbiter not in objects:
+            objects[orbiter] = set()
+        objects[orbiter].add(orbited)
+        print("{} orbits {}".format(orbiter, objects[orbiter]))
+
+count = 0
+for x in objects:
+    count += len(objects[x])
+    for y in objects[x]:
+        if y in objects:
+            count += len(objects[y])
+            for z in objects[y]:
+                if z in objects:
+                    count += len(objects[z])
+                    for a in objects[z]:
+                        if a in objects:
+                            count += len(objects[a])
+print(count)
